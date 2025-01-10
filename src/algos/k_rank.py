3dfa3ebbdb0cc01cfbc6b1991976f6b69ec508c8
notPlancha <ap9169@mau.se>
2025-01-10 12:10:18 +0100

    krank is correct, it's the combination with svm thats awful because of imbalance

--- src/algos/k_rank.py
+++ src/algos/k_rank.py
@@ -9,11 +9,14 @@ class KRank(Model):
   https://link.springer.com/content/pdf/10.1007/3-540-44795-4_13.pdf
   One versus all except that k-1 binary classifiers are trained to rank ordinal categories
   """
-
+  modelCall: Model
+  classifiers: dict[int, Model]
+  k: tuple[int, int]
+  
   def __init__(self, modelCall=HardMarginSVM):
     super().__init__()
     self.modelCall = modelCall
-    self.classifiers: list = []
+    self.classifiers = {}
     self.k = None
 
   def fit(self, X: np.ndarray, y: np.ndarray):
@@ -22,7 +25,7 @@ class KRank(Model):
       y_binary = np.array([1 if y_i > i else -1 for y_i in y])
       model = self.modelCall()
       model.fit(X, y_binary)
-      self.classifiers.append(model)
+      self.classifiers[i] = model
     super().fit()
 
   def predict(self, X: np.ndarray):
@@ -35,12 +38,12 @@ class KRank(Model):
     for x in X:
       # transform x to shape (1, n_features)
       x = x.reshape(1, -1)
-      probs: list[np.float32] = []
-      probs.append(1 - self.classifiers[0].predict_proba(x)[0])
+      probs = {}
+      probs[self.k[0]] = 1 - self.classifiers[self.k[0]].predict_proba(x)[0]
       for i in range(self.k[0] + 1, self.k[1]):
-        probs.append(self.classifiers[i - self.k[0]].predict_proba(x)[0] * (1-probs[-1]))
-      probs.append(self.classifiers[-1].predict_proba(x)[0])
-      predictions.append(np.argmax(probs) + self.k[0])
+        probs[i] = self.classifiers[i].predict_proba(x)[0] * (1 - self.classifiers[i - 1].predict_proba(x)[0])
+      probs[self.k[1]] = self.classifiers[self.k[1] - 1].predict_proba(x)[0]
+      predictions.append(max(probs, key=probs.get))
     return np.array(predictions, dtype=np.int32)
 
 if __name__ == "__main__":
@@ -50,6 +53,8 @@ if __name__ == "__main__":
   data = fetch_openml(name='wine-quality-red', version=1)
   X, y = data.data, data.target
   X, y = np.array(X), np.array(y, dtype=np.int32)
+  # normalize X
+  X = (X - X.mean(axis=0)) / X.std(axis=0)
   ic(X, y, type(X), type(y), X.shape, y.shape)
   # cache
   if not os.path.exists('krank.pkl'):
@@ -62,8 +67,7 @@ if __name__ == "__main__":
       krank = load(f)
   # print krank json
   ic(krank.__dict__)
-  ic(krank.classifiers[0].__dict__)
   predictions = krank.predict(X)
-  ic(predictions, predictions.shape)
-  
+  # print prediction counts
+  ic(predictions, predictions.shape, np.unique(predictions, return_counts=True)) 
   ic(np.mean(predictions == y))
